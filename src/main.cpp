96b43c3d7c1f610a30a1510734a525e816402901
Ruslan Byku <ruslanbyku@gmail.com>
2025-02-04 18:30:55 +0300

    Project's basic functionality is ready

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,12 @@
 #include <cassert>
+#include <chrono>
 #include <iostream>
+#include <thread>
 
 #include "moving_average.h"
 #include "random_generator.h"
+
+// #define NDEBUG
 
 static const int dataSize      = 1'000'000;
 static const int windowSizes[] = { 4, 8, 16, 32, 64, 128 };
@@ -16,11 +20,78 @@ void printVector(const std::vector<T>& data)
     std::cout << std::endl;
 };
 
+void calculateMovingAverageForDouble()
+{
+    RandomGenerator<double> randomGenerator;
+    std::vector<double>     inputData =
+        randomGenerator.generateRandomInput(dataSize); // Time: O(dataSize)
+    // printVector<double>(inputData);
+
+    for (auto windowSize : windowSizes) { // Time: O(1)
+
+        MovingAverage<double> movingAverage(windowSize); // Time: O(windowSize) ~ O(1)
+        std::vector<double>   outputData(dataSize); // Time: O(dataSize)
+
+        std::chrono::steady_clock::time_point begin =
+            std::chrono::steady_clock::now();
+        for (int idx = 0; idx < dataSize; ++idx) { // Time: O(dataSize)
+            outputData[idx] = movingAverage.calculate(inputData[idx]); // Time: O(1)
+        }
+        std::chrono::steady_clock::time_point end =
+            std::chrono::steady_clock::now();
+        // printVector<double>(outputData);
+
+        auto elapsedTime =
+            std::chrono::duration_cast<std::chrono::microseconds>(end - begin).count();
+
+        std::cerr << "Elements(double)/windowSize[" << dataSize
+                  << "/" << windowSize << "] - " << elapsedTime
+                  << " us" << std::endl;
+
+        // std::this_thread::sleep_for(std::chrono::milliseconds(100));
+    }
+}
+
+// The Time complexity is the same as for `calculateMovingAverageForDouble()`
+void calculateMovingAverageForFloat()
+{
+    RandomGenerator<float> randomGenerator;
+    std::vector<float>     inputData =
+        randomGenerator.generateRandomInput(dataSize);
+    // printVector<float>(inputData);
+
+    for (auto windowSize : windowSizes) {
+
+        MovingAverage<float> movingAverage(windowSize);
+        std::vector<float>   outputData(dataSize);
+
+        std::chrono::steady_clock::time_point begin =
+            std::chrono::steady_clock::now();
+        for (int idx = 0; idx < dataSize; ++idx) {
+            outputData[idx] = movingAverage.calculate(inputData[idx]);
+        }
+        std::chrono::steady_clock::time_point end =
+            std::chrono::steady_clock::now();
+        // printVector<float>(outputData);
+
+        auto elapsedTime = std::chrono::duration_cast<std::chrono::microseconds>(end - begin).count();
+
+        std::cerr << "Elements(float)/windowSize[" << dataSize
+                  << "/" << windowSize << "] - " << elapsedTime
+                  << " us" << std::endl;
+
+        // std::this_thread::sleep_for(std::chrono::milliseconds(100));
+    }
+}
+
 void testMovingAverage();
 
 int main(int argc, char** argv)
 {
     testMovingAverage();
+
+    calculateMovingAverageForDouble();
+    calculateMovingAverageForFloat();
 
     return 0;
 }
@@ -70,7 +141,7 @@ void testMovingAverage()
             assert(("Failed, values do not match!", outputData[i] == expectedResult[i]));
         }
 
-         std::cout << "Test for window size of " << windowSize << " passed" << std::endl;
+        std::cout << "Test for window size of " << windowSize << " passed" << std::endl;
     }
 
     {
@@ -89,7 +160,7 @@ void testMovingAverage()
             assert(("Failed, values do not match!", outputData[i] == expectedResult[i]));
         }
 
-         std::cout << "Test for window size of " << windowSize << " passed" << std::endl;
+        std::cout << "Test for window size of " << windowSize << " passed" << std::endl;
     }
 
     // Tests for window sizes 32, 64 and 128 will be the same as with 16.
